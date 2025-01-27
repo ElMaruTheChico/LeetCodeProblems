521e9c082302ce9d6369eff1f8a6ece76bd2a9d9
ElMaruTheChico <vicentechico24601@gmail.com>
2025-01-27 15:38:07 +0200

    Update file 1/27/2025, 3:38:09 PM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    employees.loc[:,'bonus'] = employees['salary'] * 2
-    return employees
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    cleanCustomers = customers.drop_duplicates(subset=['email'])
+    return cleanCustomers
     
